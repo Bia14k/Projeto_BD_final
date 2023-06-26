10c1e54e8d2c681e3d0a808a92f51fe303dde600
Maria Bianca Sousa <96076727+Bia14k@users.noreply.github.com>
2023-06-26 09:09:54 -0300

    Add files via upload

---
+++ Projetofinal/QUERYS.sql
@@ -0,0 +1,16 @@
+--SELECT * FROM Vendas;
+
+--INSERT INTO Vendas(id_cliente, id_bordado,data_venda,preco_uni,tecido,preco_total)
+--VALUES (4,3,'23-06-2023',45,'Linho',65);
+
+--UPDATE Vendas
+--SET data_venda = '25-06-2023'
+--WHERE code_venda = 7;
+
+--DELETE FROM Vendas
+--WHERE code_venda = 7;
+
+-----------Consultas Avançadas
+--SELECT SUM(preco_total), AVG(preco_total)
+--FROM Vendas;
+
